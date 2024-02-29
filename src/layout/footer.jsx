fbdb53649cdd356c0aad79c9427dc4a0e16e115e
Vladimir Melnik <nexxonyou@gmail.com>
2024-02-29 13:17:48 +0300

    final

--- src/layout/footer.jsx
+++ src/layout/footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
         <div className="footer-copyright">
             <div className="container">
                 © {new Date().getFullYear()} Мельник В.Г. (ТКБО-02-22)
-                <a className="grey-text text-lighten-4 right" href="#!">Repository</a>
+                <a className="grey-text text-lighten-4 right" href="https://github.com/MidRangeShotCreator/ReactMovie">Repository</a>
             </div>
         </div>
     </footer>
--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -4,7 +4,7 @@ function Header() {
             <div class="nav-wrapper">
                 <a href="!#" className="brand-logo">React Movies</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="!#">Repository</a></li>
+                    <li><a href="https://github.com/MidRangeShotCreator/ReactMovie">Repository</a></li>
                 </ul>
             </div>
         </div>
